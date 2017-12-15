63f7d6e0e66e876affdc40ad53ea68977475bc2a
Ian Fore <ian.fore@nih.gov>
2017-12-15 15:10:16 -0500

    Added BioprojectSampleAttributes.py

---
+++ BioprojectSampleAttributes.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+import sys
+import getopt
+import requests
+import json
+import xml.etree.ElementTree as ET
+import urllib
+
+# Retrieve the attributes of the samples for a specified BioProject and summarize
+# the values of those attributes across the project.
+# The intent is to provide a report which will help a user
+# 1. Identify the data model of the study
+# 2. Identify if the project uses a template similar to other studies
+
+ 
+def getSampleAttributes(sampleID, attList, attDetails, url):
+#	print (urllib.urlopen(url+str(sampleID)).read())
+ 	tree = ET.parse(urllib.urlopen(url+str(sampleID)))
+ 	root = tree.getroot();
+ 	for samp in root:
+ 		print ("sample:" + str(samp.get('id')))
+ 		atts = samp.find('Attributes')	
+ 		for att in atts:
+ 			aname = str(att.get('harmonized_name'))
+			if aname == 'None':
+				aname = str(att.get('attribute_name'))
+ 			if aname in attList:
+ 				attList[aname] += 1
+ 				if att.text in attDetails[aname]:
+ 					attDetails[aname][att.text] += 1
+ 				else:
+ 					attDetails[aname][att.text] = 1
+ 			else:
+ 				attList[aname] = 1
+ 				attDetails[aname] = {att.text : 1}
+
+	
+def bioprojectAttributes(bioprojectID,apikey):
+	eutils = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
+	elink = eutils + 'elink.fcgi?dbfrom=bioproject&db=biosample&retmode=json&linkname=bioproject_biosample&api_key='+apikey
+	pfetch = eutils + 'efetch.fcgi?db=bioproject&api_key='+ apikey+'&id='
+	sfetch = eutils + 'efetch.fcgi?db=biosample&api_key='+ apikey+'&id='
+	
+	# get linked samples
+	# the behavior of elink is such that the call to it will only retrieve 500 samples
+	# for now that is a good enough way to stop the program consuming resources
+	url = elink + '&id=' + bioprojectID 
+	attList = {}
+	attDetails = {}
+
+	print url
+	r = requests.get(url)
+	b = r.json()
+	d = b['linksets'][0]
+	p = d['linksetdbs'][0]
+	h = p['links']
+	
+	# call efetch for a chunk of sample ids in one go
+	# so far calling for 25 ids at once seems to be a good compromise between the number
+	# of calls that must be made and requesting too many ids which makes any call too slow
+	idchunk = 25
+ 	for i in xrange(0,len(h),idchunk):
+ 		# create a slice of ids and turn it into a comma delimited string w no spaces
+ 		# the ids are numbers - so they must also be converted to strings
+		sl = ','.join(map(str, h[i:i+idchunk])) 
+ 		getSampleAttributes(sl, attList, attDetails, sfetch)
+ 		print '____________________________________'
+ 		
+	# Summarize the attributes for this bioproject
+	uniques = {}	
+	almostUniques = {}	
+	constants = {}	
+	print 'Attribute details for BioProject ID: ' + bioprojectID
+	print 'No of samples:' + str(len(h))
+ 	print '____________________________________'
+ 	print 	'The following attributes vary across samples.'
+ 	print 	'Some may indicate the project design/model.'
+ 	print 	'Some may be sample/subject observations/measurements/data elements.'
+ 	print
+ 	for a, vList in attDetails.items():
+ 		if len(vList) == attList[a]:
+ 			uniques[a] = vList
+ 		elif 100.0*len(vList)/attList[a] > 80.0:
+ 			almostUniques[a] = vList
+ 		elif len(vList) == 1 and vList.values()[0] == len(h):
+ 			constants[a] = vList
+ 		else:
+ 			print 'Attribute:' + a + ' total:' + str(attList[a])
+ 			for v in vList:
+ 				print v, vList[v]
+ 			print '____________________________________'
+ 		
+ 	print 	'The following attributes have a unique value for each sample. '
+ 	print 	'They are therefore likely to some kind of identifier.'
+ 	for a in uniques:
+ 		print a, attList[a]
+ 	print '____________________________________'
+ 	print 	'The following attributes have a unique value for more than 80% of samples.'
+ 	print 	'They are often a subject identifier.'
+ 	for a in almostUniques:
+ 		print a, attList[a]
+ 	print '____________________________________'
+ 	print 	'The following have the same value for all samples.'
+ 	print 	'They are likely to be an attribute of the study rather than the sample'
+ 	for a, vList in constants.items() :
+ 		print a+':'+vList.keys()[0] 
+ 	print '____________________________________'
+#	work in progress - do a json dump of the attribute details	
+# 	print json.dumps(attDetails, sort_keys=True, indent=4, separators=(',', ': '))
+	
+
+def usage():
+	print sys.argv[0] +' -k apikey -b bioprojectid'
+	
+def main(argv):
+    bioprojectid = ''
+    apikey = ''
+    maxsamples = 5000
+    try:
+        opts, args = getopt.getopt(argv, "hb:k:", ["help", "bioprojectid=", "apikey="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-b", "--bioprojectid"):
+            bioprojectid = arg
+        elif opt in ("-k", "--apikey"):
+            apikey = arg
+
+    bioprojectAttributes(bioprojectid,apikey)
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+
+	
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # bio-model-discovery
 The aim of this project is to discover the data model of a biological study. The data model reflects the design of the study and is driven by the scientists who create the study. Many systems accommodate this by allowing users to specify the attributes/observations or data elements recorded in the study. It is often not clear which of these attributes are observations and which represent the factors/dimensions of the study.
+
+## BioprojectSampleAttributes.py
+Command line utility to retrieve the attributes of the samples for a specified BioProject and summarize the values of those attributes across the project.
+
+The program uses the NCBI Entrez E-utilities
+
+<https://www.ncbi.nlm.nih.gov/books/NBK25497/>
+
+The program should be called using an apikey obtained from your NCBI account settings. See the link for details.
+
+
+Note that only the first 5000 samples for a project will be retrieved.
+
+### Usage
+BioprojectSampleAttributes -k apikey -b bioprojectid
+
+### Example
+The example output for BioProject id 421626 would help a reader identify that the nine samples in the study are organized into three groups according to the "growth protocol" used. There are three replicates in each group. It also identifies that a number of attributes have the same value for all samples. It is likely that these are attributes of the study rather than the sample.
+
+```
+____________________________________
+Attribute details for BioProject ID: 421626
+No of samples:9
+____________________________________
+The following attributes vary across samples.
+Some may indicate the project design/model.
+Some may be sample/subject observations/measurements/data elements.
+
+Attribute:growth_protocol total:9
+30¦ÌM ZEA processing 3
+Do not add ZEA processing 3
+10¦ÌM ZEA processing 3
+____________________________________
+Attribute:replicate total:9
+biological replicate 1 3
+biological replicate 2 3
+biological replicate 3 3
+____________________________________
+The following have the same value for all samples.
+They are likely to be an attribute of the study rather than the sample
+strain:Kunming
+age:12 days
+dev_stage:not applicable
+tissue:Ovarian granulosa cells
+sex:female
+```
+
+
